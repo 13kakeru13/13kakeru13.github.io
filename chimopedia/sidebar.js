1def1bf3e3880e505f7d2c598819b27ca419c86d
sachimo <90051098+13kakeru13@users.noreply.github.com>
2023-07-10 16:32:50 +0900

    Create sidebar.js

---
+++ chimopedia/sidebar.js
@@ -0,0 +1,6 @@
+function putSidebar() {
+  /*サイドバ－取りつけ*/
+  var sidebarelement = document.getElementById("sidebarkun");
+  sidebarelement.outerHTML =
+    '<font size="2" color="#0645ad"><a href="/">メインページあるよ(笑)</a><br>うんち<br>おしり<br>↑をふくための紙<br>野獣先輩<br><br><font size="2" color="gray">ヘルプ</font><br><div class="さいどの棒のやつ"></div>助けて<br>井戸<br><a href="https://furafuramentaiko.github.io/Furafuramentaiko/wiki風自己紹介ページ/">こっからいろいろ借りたべ</a><br><a href="https://github.com/Furafuramentaiko">↑ありがとう</a><br></font>';
+}
