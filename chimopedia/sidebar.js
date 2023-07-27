3177b54e8edf59608fc6ead8c00854b96254d282
sachimo <90051098+13kakeru13@users.noreply.github.com>
2023-07-27 17:10:05 +0900

    Update sidebar.js

--- chimopedia/sidebar.js
+++ chimopedia/sidebar.js
@@ -2,7 +2,7 @@ function putSidebar() {
   /*サイドバ－取りつけ*/
   var sidebarelement = document.getElementById("sidebarkun");
   sidebarelement.outerHTML =
-    '<font size="2" color="#0645ad"><span id="sbmenu"><a href="/chimopedia">メインページあるよ(笑)</a><br>うんち<br>おしり<br>↑をふくための紙<br>野獣先輩</span><br><br><font size="2" color="gray" class="さいどの棒のやつ">ヘルプ</font><br><span id="sbhelp">助けて<br>井戸</span><br><font size="2" color="gray">クレジット</font><span id="sbcredit"><a href="https://furafuramentaiko.github.io/Furafuramentaiko/wiki風自己紹介ページ/">こっからいろいろ借りたべ</a><br><a href="https://github.com/Furafuramentaiko">↑ありがとう</a></span><br><br><font size="2" color="gray" class="さいどの棒のやつ">他言語版</font><span id="sblanguages"></span></font>';
+    '<font size="2" color="#0645ad"><div id="sbmenu"><a href="/chimopedia">メインページあるよ(笑)</a><br>うんち<br>おしり<br>↑をふくための紙<br>野獣先輩</div><br><font size="2" color="gray" class="さいどの棒のやつ">ヘルプ</font><div id="sbhelp">助けて<br>井戸</div><br><font size="2" color="gray">クレジット</font><div id="sbcredit"><a href="https://furafuramentaiko.github.io/Furafuramentaiko/wiki風自己紹介ページ/">こっからいろいろ借りたべ</a><br><a href="https://github.com/Furafuramentaiko">↑ありがとう</a></div><br><font size="2" color="gray" class="さいどの棒のやつ">他言語版</font><div id="sblanguages"></div></font>';
 }
 
 function otherLanguage(otherlang){
