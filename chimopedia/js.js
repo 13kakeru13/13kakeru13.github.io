ff4fa45dd995a7105a80c0046c81e3cb55b8d55a
sachimo <90051098+13kakeru13@users.noreply.github.com>
2023-08-16 16:26:49 +0900

    Update js.js

--- chimopedia/js.js
+++ chimopedia/js.js
@@ -5,12 +5,12 @@ function putSidebar() {
     '<font size="2" color="#0645ad"><div id="sbmenu"><a href="/chimopedia">メインページあるよ(笑)</a><br>うんち<br>おしり<br>↑をふくための紙<br>野獣先輩</div><br><font size="2" color="gray" class="さいどの棒のやつ">ヘルプ</font><div id="sbhelp">助けて<br>井戸</div><br><font size="2" color="gray">クレジット</font><div id="sbcredit"><a href="https://furafuramentaiko.github.io/Furafuramentaiko/wiki風自己紹介ページ/">こっからいろいろ借りたべ</a><br><a href="https://github.com/Furafuramentaiko">↑ありがとう</a></div><br><font size="2" color="gray" class="さいどの棒のやつ">他言語版</font><div id="sblanguages"></div></font>';
 }
 
-function otherLanguage(otherlang){
-   var sidebarelement = document.getElementById("sblangu");
-  sidebarelement.outerHTML = otherlang
-}
-
 function titlename(){
   var site = location.href.split('/');
   document.title = decodeURI(site[5]) + " - chimopedia";
+}
+
+function otherLanguage(otherlang){
+   var sidebarelement = document.getElementById("sblanguages").innerHTML;
+   sidebarelement=sidebarelement+"<a href='"+otherlang+".html'>"+otherlang+"</a><br>";
 }
