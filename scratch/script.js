472ad926bf691033f1d0046038d1a989780a86ba
sachimo <90051098+13kakeru13@users.noreply.github.com>
2023-08-24 17:12:48 +0900

    Update script.js

--- scratch/script.js
+++ scratch/script.js
@@ -1,5 +1,5 @@
 //ヘッダー追加
-document.getElementById("header").outerHTML =
+document.getElementById("header").innerHTML =
     '<a href="https://scratch.mit.edu">scratchのアカウント</a></h3><h3><a href="/aboutme.html">さちｍｋについて</a></h3><h3><a href="/scratch/link">相互リンク</a></h3><h3><a href="/scratch/diary">日記</a></h3>';
 
 
