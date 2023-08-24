b6e9a834bbb18aa19a03172ae277c68d6c7ee08c
sachimo <90051098+13kakeru13@users.noreply.github.com>
2023-08-24 17:46:50 +0900

    Update script.js

--- scratch/script.js
+++ scratch/script.js
@@ -1,6 +1,8 @@
 //ヘッダー追加
 document.getElementById("header").innerHTML =
-    '<h3><a href="https://scratch.mit.edu">Scratch</a></h3><h3><a href="/aboutme.html">さちｍｋについて</a></h3><h3><a href="/scratch/diary">日記</a></h3><h3><a href="/scratch/link">相互リンク</a></h3>';
+    '<h3><a id="myaccount" href="https://scratch.mit.edu">Scratch</a></h3><h3><a href="/aboutme.html">さちｍｋについて</a></h3><h3><a href="/scratch/diary">日記</a></h3><h3><a href="/scratch/link">相互リンク</a></h3>';
 
 
 //ユザネが入るところを自分のユザネに置き換える
+var myname="SatimK_w2";
+document.getElementById("myaccount").outerHTML='<a id="myaccount" href="https://scratch.mit.edu/users/'+myname+'/">Scratch</a>';
